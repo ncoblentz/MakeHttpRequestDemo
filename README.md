67de33a1a6e6aefe1ce151624a7249273ebb92e5
Nick Coblentz <nick.coblentz@gmail.com>
2024-08-26 13:19:47 -0500

    initial base project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Make HTTP Request Demo
+
+This project demonstrates how to:
+1. Create a right-click context menu, with options for running various test cases that make new HTTP requests. Those results are viewable within the Logger tab of Burp Suite
+
+## Setup
+
+This project was initially created using the template found at: https://github.com/ncoblentz/KotlinBurpExtensionBase. That template's README.md describes how to:
+- Build this and other projects based on the template
+- Load the built jar file in Burp Suite
+- Debug Burp Suite extensions using IntelliJ
+- Provides links to documentation for building Burp Suite Plugins
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    // Provides Kotlin Language Support
+    // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
+    kotlin("jvm") version "2.0.20"
+
+    // Provides the shadowJar task in Gradle
+    // https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+
+}
+
+group = "com.nickcoblentz.montoya.demo"
+version = "0.1.0"
+
+repositories {
+    //add maven local in case you want to build some reusable libraries and host them within your home directory
+    mavenLocal()
+
+    mavenCentral()
+
+    // Enable these if you want to use https://github.com/ncoblentz/BurpMontoyaLibrary
+    // Add two specific GitHub repositories in which maven packages can be found through jitpack.io
+    /*
+    maven(url="https://jitpack.io") {
+        content {
+            includeGroup("com.github.milchreis")
+            includeGroup("com.github.ncoblentz")
+        }
+    }
+    */
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    // Include the Montoya API from Maven Central:
+    // https://central.sonatype.com/artifact/net.portswigger.burp.extensions/montoya-api
+    // Check for latest version: https://central.sonatype.com/artifact/net.portswigger.burp.extensions/montoya-api/versions
+    implementation("net.portswigger.burp.extensions:montoya-api:2024.7")
+
+    // Enable these if you want to use https://github.com/ncoblentz/BurpMontoyaLibrary
+    //implementation("com.github.ncoblentz:BurpMontoyaLibrary:0.1.22")
+    //https://github.com/Milchreis/UiBooster/releases (a dependency of BurpMontoyaLibrary)
+    //implementation("com.github.milchreis:uibooster:1.21.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
